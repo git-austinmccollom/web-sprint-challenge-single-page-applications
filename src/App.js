818ff753dd42036fcbe764cfa23240185b86054b
git-austinmccollom <austinmccollom@gmail.com>
2020-08-21 11:13:50 -0500

    basic form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Link, Route, Redirect } from 'react-router-dom';
 import Pizza from './components/Pizza'
+import './App.css'
 
 const App = () => {
   return (
     <>
+      <div className='App-header'>
       <h1>Lambda Eats</h1>
-        <Link to='/pizza'>Pizza</Link>
-        <Route path='/pizza'>
+        <Link className='App-link' to='/'>Home</Link>
+        <br></br>
+        <Link className='App-link' to='/pizza'>Pizza</Link>
+        <Route exact path='/'>
+          <Redirect to='/' />
+        </Route>
+      </div>
+        <Route exact path='/pizza'>
           <Pizza />
         </Route>
     </>
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,8 +1,69 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+
+const FormBox = styled.form`
+    width: 50%;
+    margin: auto;
+    border: 2px solid black;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    label {
+        margin: 5px 0;
+    }
+    input {
+        margin: 0 5px;
+    }
+    select {
+        margin: 0 5px;
+    }
+    .innerForm {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .errorDiv {
+        color: red;
+    }
+    .inlineDiv {
+        display: flex;
+    }
+`
 
 export default function Pizza(props) {
 
     return (
-        <p>Pizza!</p>
+        <FormBox>
+            <div className='innerForm'>
+                <h1>Order Pizza!</h1>
+                <div className='inlineDiv'>
+                    <label>Name:
+                        <input
+                        name='name'
+                        type='text'
+                        placeholder='Name'
+                        // value={formValues.name}
+                        // onChange={onInputChange}
+                        />
+                    </label>
+                </div>
+                <div className='inlineDiv'>
+                    <label>Size:
+                        <select name='pizza-size'>
+                            <option value='small'>Small</option>
+                            <option value='medium'>Medium</option>
+                            <option value='large'>Large</option>
+                        </select>
+                    </label>
+                </div>
+                <h3>Pick your toppings</h3>
+                <div className='inlineDiv'>
+                    <input type='checkbox' name='peppers'/>
+                    <label htmlFor="peppers">Peppers</label>
+                </div>
+                {/* <button disabled={disabled}>submit</button> */}
+            </div>
+        </FormBox>
     )
 }
